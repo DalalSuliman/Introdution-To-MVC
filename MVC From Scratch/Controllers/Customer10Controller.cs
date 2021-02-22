71751d129b212c091f32622d21319cb5dbba6402
Dalal Suliman <Dalalali259@gmail.com>
2021-02-22 20:42:58 +0200

    Lesson 10

---
+++ MVC From Scratch/Controllers/Customer10Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Customer10Controller : Controller
+    {
+        // GET: Customer10
+        public ActionResult DisplayCustomer()
+        {
+            var customer = new Customer10Model(1, "Dalal", 23, "sweleih");
+
+            return View(customer);
+        }
+
+        public ActionResult Create()
+        {   
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer10Model cust)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("DisplayCustomer");
+            }
+           
+            return View(cust);
+        }
+    }
+}
---
+++ MVC From Scratch/Models/Customer10Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Customer10Model
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Enter Your Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+        public Customer10Model()
+        {
+        }
+        public Customer10Model(int id, string name, int age, string address)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+    }
+}
